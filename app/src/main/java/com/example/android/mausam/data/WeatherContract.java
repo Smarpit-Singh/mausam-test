93fc1d9b2a25027d0b84a727e9271923d921498c
Smarpit Singh <smarpitsingh560@gmail.com>
2018-07-17 14:51:08 +0530

    database created

---
+++ app/src/main/java/com/example/android/mausam/data/WeatherContract.java
@@ -0,0 +1,20 @@
+package com.example.android.mausam.data;
+
+import android.provider.BaseColumns;
+
+public class WeatherContract {
+
+    public static final class WeatherEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "weather";
+        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_WEATHER_ID = "weather_id";
+        public static final String COLUMN_MIN_TEMP = "min";
+        public static final String COLUMN_MAX_TEMP = "max";
+        public static final String COLUMN_HUMIDITY = "humidity";
+        public static final String COLUMN_PRESSURE = "pressure";
+        public static final String COLUMN_WIND_SPEED = "wind";
+        public static final String COLUMN_DEGREES = "degrees";
+
+    }
+}
